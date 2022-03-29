4756391de47fd4dec7fb47e16ed986cd48364653
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-03-29 23:25:02 +0530

    Time: 11 ms (41.04%), Space: 14 MB (71.85%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        set<int>st(nums.begin(),nums.end());
+       /* set<int>st(nums.begin(),nums.end());
        if(st.find(target)!=st.end())return true;
-        return false;
-            
+        return false;*/
+        // two pointer approach 
+        int l=0,r=nums.size()-1;
+        while(l<=r)
+        {
+            if(nums[l]==target || nums[r]==target)return true;
+            l++;r--;
+        }
+           return false ; 
     }
 };
