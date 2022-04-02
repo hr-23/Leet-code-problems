4d570c2c46af99a7c3a750bf2608f69ab1165c9c
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-04-02 15:45:51 +0530

    Time: 31 ms (57.84%), Space: 19.3 MB (57.69%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-    vector<int>v;
-        for(int i=0;i<nums.size();i++)
-            if(nums[i]!=0)v.push_back(nums[i]);
-        for(int i=0;i<v.size();i++)
-            nums[i]=v[i];
-        for(int i=v.size();i<nums.size();i++)
-            nums[i]=0;
-        return ;
+    // move all the zeroes to the right of the vector
+     int j=0;
+       for(int i=0;i<nums.size();i++)
+           if(nums[i]!=0)
+               nums[j++]=nums[i];
+        for(;j<nums.size();j++)nums[j]=0;return ;
         
     }
 };
