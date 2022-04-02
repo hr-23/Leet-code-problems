fbb9ea10742b285f0daffc39b637d6170341df6e
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-04-02 11:00:08 +0530

    Time: 11 ms (67.03%), Space: 15.4 MB (59.17%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -3,14 +3,14 @@ public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
         stack<int>s;
         int j=0;
-          for(auto it:pushed)
-          {
-              s.push(it);
-              while(!s.empty() && s.top()==popped[j])
-              {
-                  s.pop();j++;
-               }
-          }
+        for(auto i:pushed)
+        {
+            s.push(i);
+            while(!s.empty() && s.top()==popped[j])
+            {
+                s.pop();j++;
+            }
+        }
         if(s.empty())return true;
         return false;
     }
