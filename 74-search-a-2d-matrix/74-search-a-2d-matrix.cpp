1ea64885466e565141cd34c77b2dca0dfa0a1a78
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-04-14 11:27:02 +0530

    Time: 10 ms (18.62%), Space: 9.6 MB (5.77%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i=0;i<matrix.size();i++)
-        {
-            set<int>s(matrix[i].begin(),matrix[i].end());
-            if(s.find(target)!=s.end())
-                return true;
-        }
+      for(int i=0;i<matrix.size();i++)
+      {
+          if(binary_search(matrix[i].begin(),matrix[i].end(),target))
+              return true;
+      }
         return false;
     }
 };
