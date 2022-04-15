eb2a1cca2f498b78ca7a1257c2055b535fa59878
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-04-15 12:01:52 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (51.27%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -7,19 +7,16 @@ public:
         if(o=='(' && c==')')return true;
         return false;
     }
+    // think of a case where the first case is invalid char and this should not be empty
     bool isValid(string s) {
      stack<char>st;
-        for(int i=0;i<s.length();i++)
-        {
-            if(s[i]=='(' || s[i]=='[' || s[i]=='{')
-                st.push(s[i]);
-           else{  if(st.empty())return false;
-                if(isvalid(st.top(),s[i]))
-                    st.pop();
-                else return false;
-               }
-
-        }
+      for(int i=0;i<s.length();i++)
+      {
+          if(s[i]=='[' || s[i]=='{' || s[i]=='(')st.push(s[i]);
+          else if(!st.empty() && isvalid(st.top(),s[i]))
+              st.pop();
+          else return false;
+      }
         if(st.empty())return true;
         return false;
     }
