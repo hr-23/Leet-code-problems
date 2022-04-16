c0f494d28eea89a18908320547c83d07a25d559b
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-04-16 15:13:58 +0530

    Time: 15 ms (73.01%), Space: 12 MB (13.08%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -5,20 +5,18 @@ public:
         // we have to do it in o(n) time complexity that is using hash table (unordered map)
     // the logic is to take a hash table then calculate target-nums[i] if it is there in the map then return the index of the current pointer and the ans value .
        //recall the find function in map  -it only accepts keys
-       map<int,int>m;
-  vector<int>v(2);
-   for(int i=0;i<nums.size();i++)
-   {
-       if(m.find(target-nums[i])!=m.end())
-       {
-           v[0]=m[target-nums[i]];
-           v[1]=i;
-           return v;
-       }
-       m[nums[i]]=i;
-   }
-        return v;
-      
+ unordered_map<int,int>m;
+        vector<int>v(2);
+        for(int i=0;i<nums.size();i++)
+        {
+            if(m.find(target-nums[i])!=m.end())
+            {
+                v[0]=i;
+                v[1]=m[target-nums[i]];
+            }
+            m[nums[i]]=i;
+        }
+      return v;
     }
     
 };
