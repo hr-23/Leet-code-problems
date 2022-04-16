8acccfec2ec413c2dc8bbfb1fea138e21bd89a25
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-04-16 15:20:15 +0530

    Time: 13 ms (76.80%), Space: 15.7 MB (43.74%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -3,13 +3,13 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
       int i=0,j=nums.size()-1;
         // as that the array is sorted we have to think tha approach of binary search ,so that also recall the same sum in which we used map to find the target
-        while(i<j)
-        {
-            if(nums[i]+nums[j]==target)
-                return {i+1,j+1};
-            else if(nums[i]+nums[j]>target)j--;
-            else i++;
-        }
-        return {0,0};
+       while(i<j)
+       {
+           int sum=nums[i]+nums[j];
+           if(sum==target)return {i+1,j+1};
+           else if(sum>target)j--;
+           else i++;
+       }
+        return {i+1,j+1};
     }
 };
