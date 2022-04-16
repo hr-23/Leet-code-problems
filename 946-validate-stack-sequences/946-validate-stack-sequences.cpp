056bfba071524e4030954f67ac62f7d1e69ab844
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-04-16 16:59:30 +0530

    Time: 14 ms (43.23%), Space: 15.4 MB (59.15%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
        stack<int>s;int j=0;
+        // first try to visualize the algorithm and then recall the use of while function 
+        // first push the element and then look for popping up 
         for(auto &it:pushed)
         {
             s.push(it);
