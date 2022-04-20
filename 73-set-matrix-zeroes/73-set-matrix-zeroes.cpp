61afe12074edecdc9c909479ec113298218134c6
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-04-20 10:04:27 +0530

    Time: 15 ms (76.49%), Space: 13.3 MB (54.71%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,26 +3,32 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
 // the optimal approach for this question will be to use the first row and first coloumn as to set the row and coloumn to zero if an element found is 0 
 // we use bool to check whether the row and coloumn element became zero or not and then if true then we will set all elements to 0 
-        bool row = false, col = false;
-        for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[0].size(); j++){
-                if(matrix[i][j] == 0) {
-                    if(i == 0) row = true;
-                    if(j == 0) col = true;
-                    matrix[0][j] = matrix[i][0] = 0;
+       bool row=false,col=false;
+        for(int i=0;i<matrix.size();i++)
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    if(i==0)row=true;
+                    if(j==0)col=true;
+                    matrix[0][j]=matrix[i][0]=0;
                 }
             }
+        for(int i=1;i<matrix.size();i++)
+            for(int j=1;j<matrix[0].size();j++){
+            if(matrix[i][0]==0 || matrix[0][j]==0)matrix[i][j]=0;
+                }
+        if(row)
+        {
+            for(int i=0;i<matrix[0].size();i++)
+                matrix[0][i]=0;
         }
-        for(int i = 1; i < matrix.size(); i++){
-            for(int j = 1; j < matrix[0].size(); j++){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+        if(col)
+        {
+            for(int i=0;i<matrix.size();i++)
+            {
+                matrix[i][0]=0;
             }
-        }
-        if(col){
-            for(int i = 0; i < matrix.size(); i++) matrix[i][0] = 0;
-        }
-        if(row){
-            for(int j = 0; j < matrix[0].size(); j++) matrix[0][j] = 0;
         }
     }
 };
