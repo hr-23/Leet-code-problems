714fae59b82249267968cafdc1530779dce59128
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-04-20 20:50:14 +0530

    Time: 36 ms (87.11%), Space: 36.2 MB (22.93%) - LeetHub

---
+++ 485-max-consecutive-ones/485-max-consecutive-ones.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int maxi=0,sum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            if(nums[i]==0)
+                sum=0;
+             maxi=max(sum,maxi);
+        }
+    return maxi;
+    }
+};
