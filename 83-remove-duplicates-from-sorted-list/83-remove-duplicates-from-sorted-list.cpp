937f34fdc2b2178972033c73d8ff8e90f7abd85a
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-04-21 10:28:44 +0530

    Time: 9 ms (79.90%), Space: 11.7 MB (36.34%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
@@ -11,10 +11,7 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-// always remember to write the condition about edge cases like when head is NULL
-        if(head==NULL)return head;
-    // initialize two pointers p and t which points to current and next of head and a pre which is the value of the p;
-        ListNode* cur = head;
+   ListNode* cur = head;
         while(cur) {
         	while(cur->next && cur->val == cur->next->val) {
         		cur->next = cur->next->next;
