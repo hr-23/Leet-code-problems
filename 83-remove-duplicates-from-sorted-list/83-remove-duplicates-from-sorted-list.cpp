8580d3930e69fd491cb63e59f7999b3580e61968
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-04-21 10:25:46 +0530

    Time: 13 ms (56.35%), Space: 11.5 MB (79.88%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
@@ -14,20 +14,13 @@ public:
 // always remember to write the condition about edge cases like when head is NULL
         if(head==NULL)return head;
     // initialize two pointers p and t which points to current and next of head and a pre which is the value of the p;
-        ListNode* p=head;
-        ListNode* q=head->next;
-        int pre=head->val;
-        while(q)
-        {
-            if(pre!=q->val)
-            {
-              p->next=q;
-                pre=q->val;
-                p=p->next;
-            }
-            q=q->next;
+        ListNode* cur = head;
+        while(cur) {
+        	while(cur->next && cur->val == cur->next->val) {
+        		cur->next = cur->next->next;
+        	}
+        	cur = cur->next;
         }
-        p->next=NULL;
         return head;
     }
 };
