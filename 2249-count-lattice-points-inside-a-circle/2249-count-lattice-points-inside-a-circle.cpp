b0aae2d504d5e09c78cc66dc8a8627d7d32291b3
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-04-25 20:52:56 +0530

    Time: 1062 ms (45.45%), Space: 25 MB (18.18%) - LeetHub

--- 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.cpp
+++ 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int countLatticePoints(vector<vector<int>>& v) {
-        int n = v.size();
-        set<pair<int,int>> s;
-        for(int i=0;i<n;i++){
-            int a=v[i][0],b=v[i][1],r=v[i][2];
-            for(int x=-r;x<=r;x++){
-                for(int y=-r;y<=r;y++){
-                    if((x*x)+(y*y)<=(r*r)){
+        // we have to find out whether the coordinnates are present inside the circle or not
+        // treaverse through -r to r in the x direction and -r to r in the y direction covering the whole coordinate systems and find whether it si presesnt in the circle or not 
+        set<pair<int,int>>s;
+        for(int i=0;i<v.size();i++)
+        {
+            int a=v[i][0],b=v[i][1];int r=v[i][2];
+            for(int x=-r;x<=r;x++)
+                for(int y=-r;y<=r;y++)
+                    if((x*x)+(y*y)<=(r*r))
                         s.insert({x+a,y+b});
-                    }
-                }
-            }
         }
-        int ans = s.size();
-        return ans;
+        return s.size();
     }
 };
