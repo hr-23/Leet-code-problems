1cda2fa1ced4fa52b52c68ae2302ddeda165a9d7
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-04-27 21:34:58 +0530

    Time: 4 ms (61.16%), Space: 6.8 MB (25.86%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        int j=0;
+        for(int i=0;i<t.length();i++)
+        {
+            if(t[i]!=s[j])
+                return t[i];
+            else 
+              j++;
+        }
+        return 'a';
+    }
+};
