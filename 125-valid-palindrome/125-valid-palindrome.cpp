4e0e6878c22dcba638c948840ea8b3c2ea2401cc
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-06-10 09:58:34 +0530

    Time: 6 ms (69.62%), Space: 7.3 MB (75.07%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-string t;
-        for(int i=0;i<s.length();i++)
-            if(s[i]<=90 && s[i]>=65)
-            {
-                s[i]+=32;
-                t+=s[i];
-            }
-            else if((s[i]>=97 && s[i]<=122) ||(s[i]<='9' && s[i]>='0' ))
-                t+=s[i];
-        string p=t;
-        reverse(t.begin(),t.end());
-        if(p==t)return true;
-        return false;
+         for(int i=0,j=s.size()-1;i<j;i++,j--)
+         {
+             while(isalnum(s[i])==false && i<j)i++;
+             while(isalnum(s[j])==false && i<j)j--;
+             if(tolower(s[i])!=tolower(s[j]))return false;
+         }
+        return true;
     }
 };
