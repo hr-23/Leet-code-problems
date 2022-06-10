747ea34e45d8acea5a9fc5239f0442099fc9a0a9
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-06-10 18:01:29 +0530

    Time: 39 ms (33.30%), Space: 10.8 MB (26.11%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-     // sliding window concept 
+        // sliding window concept 
+        int i=0,j=0,n=s.length(),ans=0;
         unordered_set<char>st;
-        int i=0,j=0,n=s.size(),ans=0;
         while(i<n && j<n)
         {
-            if(st.find(s[j])==st.end())
-            {
-                st.insert(s[j++]);
-                ans=max(ans,j-i);
-            }
-            else 
-                st.erase(s[i++]);
+             if(st.find(s[j])==st.end())
+             {
+                 st.insert(s[j++]);
+                     ans=max(ans,j-i);
+              }
+            else st.erase(s[i++]);
         }
-        return ans;
+return ans;
     }
 };
