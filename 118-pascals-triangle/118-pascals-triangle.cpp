10cfb3108ff09c910b8c3bdd34e5c1b51d274fca
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-07-19 14:49:58 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        // first analyze the question 
-        // you are giving up too fast ,so spend a dedicated time for this and then solve 
-        // the approach is that we have to take a 2d vector and then traverse through the no of rows required and create a new vector initialized with 1 and now apply the following algorithm ,recall once ,imagine in mind 
-      vector<vector<int>> ret;
-	for (int i = 0; i < numRows; i++) {
-		vector<int> row(i + 1, 1);
-		for (int j = 1; j < i; j++) {
-			row[j] = ret[i - 1][j] + ret[i - 1][j - 1];
-		}
-		ret.push_back(row);
-	}
-	return ret;
+    vector<vector<int>> generate(int n) {
+       vector<vector<int>>v;
+        for(int i=0;i<n;i++)
+        {
+            vector<int>v1(i+1,1);
+            for(int j=1;j<i;j++)
+            {
+                v1[j]=v[i-1][j]+v[i-1][j-1];
+            }
+            v.push_back(v1);
+        }
+        return v;
     }
 };
