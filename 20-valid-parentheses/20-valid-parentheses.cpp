f6cf323f70beec9cd278845551e1eee3117f2a2b
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-08-04 00:36:27 +0530

    Time: 4 ms (32.70%), Space: 6.3 MB (50.94%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    bool isvalid(char o,char c)
+    bool ispair(char o,char c)
     {
         if(o=='(' && c==')')return true;
+        if(o=='[' && c==']')return true;
         if(o=='{' && c=='}')return true;
-         if(o=='[' && c==']')return true;
         return false;
     }
     bool isValid(string s) {
-      stack<char>st;
-      for(int i=0;i<s.length();i++)
-      {
-          if(s[i]=='[' || s[i]=='{' || s[i]=='(')st.push(s[i]);
-          else if(!st.empty() && isvalid(st.top(),s[i]))
-              st.pop();
-          else return false;
-      }
+stack<char>st;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{')st.push(s[i]);
+            else if(!st.empty() && ispair(st.top(),s[i]))st.pop();
+            else return false;
+        }
         if(st.empty())return true;
         return false;
     }
