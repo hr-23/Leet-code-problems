154ee2e22694a4ed5450a6489394b99bb5cb751e
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-08-11 01:59:06 +0530

    Time: 8 ms (94.13%), Space: 7.9 MB (81.17%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,9 +9,8 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-       if(head==NULL)return false;
-        // like the same we did for to find the middle of the linked list  using two pointers fast and slow ,do the same here also initialize two pointers fast and slow and if both of them meet at the same node then there is the loop ,else there will be no loop ,another method is to use unordered set/unordred map.
-        ListNode* fast=head;
+        if(head==NULL)return false;
+      ListNode* fast=head;
         ListNode* slow=head;
         while(fast && fast->next)
         {
