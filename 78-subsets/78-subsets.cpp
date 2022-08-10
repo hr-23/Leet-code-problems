7764d50ad4bec42d52afa7fe6c1062480678f630
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-08-11 01:22:57 +0530

    Time: 7 ms (35.77%), Space: 7.2 MB (57.04%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void print(int ind,vector<int>&v,vector<int>nums,int n)
+    // recall all the recusrion going on here and the technique of poushing and then poping back
+    vector<vector<int>>v;
+    void print(int ind,vector<int>&nums,vector<int>&ds,int n)
     {
-        if(ind==n)
+        if(ind ==n)
         {
-            ans.push_back(v);return;
+            v.push_back(ds);
+            return;
         }
-        v.push_back(nums[ind]);
-        print(ind+1,v,nums,n);
-        v.pop_back();
-        print(ind+1,v,nums,n);
+        ds.push_back(nums[ind]);
+        print(ind+1,nums,ds,n);
+        ds.pop_back();
+        print(ind+1,nums,ds,n);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>v;
-        print(0,v,nums,nums.size());
-      return ans;
+        vector<int>ds;
+        print(0,nums,ds,nums.size());
+        return v;
     }
 };
