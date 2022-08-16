6b6342442c3e79219a66b63900f1b454fe2c1897
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-08-16 09:37:30 +0530

    Time: 28 ms (86.40%), Space: 24.1 MB (52.33%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-      int c=count(nums.begin(),nums.end(),0);
-      if(c==0)
-      {
-          int s=1;
-          for(int i=0;i<nums.size();i++)s*=nums[i];
-          for(int i=0;i<nums.size();i++)nums[i]=s/nums[i];
-          return nums;
-      }
-        if(c==1)
+int c=count(nums.begin(),nums.end(),0);
+        if(c>=2)
         {
-            int s=1;
-            for(int i=0;i<nums.size();i++)
-            {
-                if(nums[i]!=0)s*=nums[i];
-            }
-            for(int i=0;i<nums.size();i++)
-            {
-                if(nums[i]!=0)nums[i]=0;
-                else nums[i]=s;
-            }
+           for(int i=0;i<nums.size();i++)nums[i]=0;
             return nums;
         }
-        for(int i=0;i<nums.size();i++)
-        {
-            nums[i]=0;
-        }
+         if(c==1)
+         {
+             int sum=1;
+              for(int i=0;i<nums.size();i++){
+                  if(nums[i]!=0)
+                sum*=nums[i];}
+             for(int i=0;i<nums.size();i++)
+             {
+                 if(nums[i]!=0)nums[i]=0;
+                 else nums[i]=sum;
+             }
+return nums;
+         }
+       int sum=1;
+      for(int i=0;i<nums.size();i++)
+          sum*=nums[i];
+       for(int i=0;i<nums.size();i++)
+           nums[i]=sum/nums[i];
         return nums;
     }
 };
