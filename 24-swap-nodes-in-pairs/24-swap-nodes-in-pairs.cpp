66d90296426e126d4e01ef338efedcbe572f4c8a
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-08-20 17:34:32 +0530

    Time: 8 ms (16.50%), Space: 7.6 MB (54.82%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -14,12 +14,10 @@ public:
     {
         if(head==NULL)return ;
         int t=head->val;
-   if(head->next!=NULL)
+   if(head->next!=NULL){
         head->val=head->next->val;
-      if(head->next!=NULL)
         head->next->val=t;
-      if(head->next!=NULL)
-        swap(head->next->next);
+        swap(head->next->next);}
     }
     ListNode* swapPairs(ListNode* head) {
         swap(head);
