14ed039a56de787e350363c446accd2bbdb0e891
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-08-20 17:00:39 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (70.35%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
        if(n==1)return true;
-       if(n%2!=0 || n<=0)return false;
+        if(n<=0 || n%2!=0)return false;
         return isPowerOfTwo(n/2);
     }
 };
