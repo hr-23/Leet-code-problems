b71f255bce3de2a5c8f621b265bf7316d63155a2
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-08-20 17:18:42 +0530

    Time: 622 ms (5.01%), Space: 148.7 MB (5.06%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,16 +10,26 @@
  */
 class Solution {
 public:
+   void insert_at_front(ListNode** head2,int x)
+{
+    ListNode* temp=new ListNode();
+    temp->val=x;
+    temp->next=*head2;
+    *head2=temp;
+}
     bool isPalindrome(ListNode* head) {
-        vector<int>v;
+          ListNode* head2;
         ListNode* n=head;
-        while(n!=NULL)
+        while(n)
         {
-            v.emplace_back(n->val);n=n->next;
+            insert_at_front(&head2,n->val);n=n->next;
         }
-        vector<int>s(v.begin(),v.end());
-        reverse(v.begin(),v.end());
-        if(s==v)return true;
-        return false;
+        ListNode* t=head;
+        ListNode* p=head2;
+        while(t)
+        {
+            if(t->val!=p->val)return false;t=t->next;p=p->next;
+        }
+        return true;
     }
 };
