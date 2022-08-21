fd2530d4795c150df622064344045de825b8b35f
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-08-21 09:22:15 +0530

    Time: 6 ms (53.96%), Space: 8.2 MB (38.17%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
 set<vector<int>>s;
-    void print(int ind,int n,vector<int>&nums,vector<int>&ds)
+    void print(vector<int>&ds,vector<int>&nums,int n,int ind)
     {
         if(ind==n)
         {
-            s.insert(ds);return;
+            s.insert(ds);
+            return;
         }
         ds.push_back(nums[ind]);
-        print(ind+1,n,nums,ds);
+        print(ds,nums,n,ind+1);
         ds.pop_back();
-          print(ind+1,n,nums,ds);
+        print(ds,nums,n,ind+1);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<int>ds;
-        int n=nums.size();
+       vector<int>ds;
         sort(nums.begin(),nums.end());
-        // this sort plays a key role ,know what is it ?
-          print(0,n,nums,ds);
-      vector<vector<int>>v(s.begin(),s.end());
+        print(ds,nums,nums.size(),0);
+        vector<vector<int>>v;
+        for(auto it:s)
+            v.push_back(it);
         return v;
     }
 };
