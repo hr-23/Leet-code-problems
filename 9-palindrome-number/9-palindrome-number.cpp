3c6f1c1ef146a671293333c2cd1e95b82e690ac7
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-08-22 01:15:16 +0530

    Time: 19 ms (67.17%), Space: 5.9 MB (74.72%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0)return false;
-         vector<int>v;
-        while(x)
+       if(x<0 || (x!=0 && x%10==0))return false;
+        int sum=0;
+        while(x>sum)
         {
-            int p=x%10;
-             v.emplace_back(p);x/=10;
+            sum = sum*10+x%10;
+            x = x/10;
         }
-        vector<int>s(v.begin(),v.end());
-        reverse(v.begin(),v.end());
-        if(s==v)return true;
-        return false;
+        return (x==sum)||(x==sum/10);
     }
 };
