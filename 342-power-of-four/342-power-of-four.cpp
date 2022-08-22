8a806e0840022caf673bf53f7b676cae8a7e46dc
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-08-22 11:22:20 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (88.37%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool isPowerOfFour(int num) {
-             return num > 0 && (num & (num - 1)) == 0 && (num - 1) % 3 == 0; 
+        // first if the number is negative return false 
+        // and we know that for checking the power of two ,we do the bitwise operaio n of n&n-1 so that if it is a power of two it becomes 0 
+    // and if it is power of four , it must also be power of two m and for power of four , the power-1 is divisible by 3 , once analyze 
+             
+        return num>0 && (num&(num-1))==0 && (num-1)%3==0;
     }
 };
