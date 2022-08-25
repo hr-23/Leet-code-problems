6d9e1610528546874fd28c133d38b082f4480776
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-08-25 10:44:57 +0530

    Time: 37 ms (31.12%), Space: 9 MB (15.41%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool canConstruct(string a, string b) {
-       int count[26]={0};
-        for(auto it:b)count[it-'a']++;
-        for(auto it:a)
-        {
-            if(count[it-'a']-- <=0)return false;
-        }
+       unordered_map<char,int>m1,m2;
+        for(auto it:a)m1[it]++;
+        for(auto it:b)m2[it]++;
+        for(int i=0;i<a.length();i++)
+            if(m1[a[i]]>m2[a[i]])return false;
         return true;
     }
 };
