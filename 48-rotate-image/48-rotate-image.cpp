7ee5975b02e7ea76f391edc64d1af56bf1c07eb1
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-08-30 09:28:49 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (33.50%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        // the commoon methodology for nxn matrix traversals are as follows for clockwise and anti clockwise directions 
-        // for clockwise direction reverse the matrix from bottom to top and then symmetrically swap them 
-        //for anti clockwise direction reverse the matrix from left to right and then symmetrically swap them 
-        reverse(matrix.begin(),matrix.end());
-        for(int i=0;i<matrix.size();i++)
-            for(int j=i+1;j<matrix[0].size();j++)
-                swap(matrix[i][j],matrix[j][i]);
-        return;
+       for(int i=0;i<matrix[0].size();i++)
+       {
+           for(int j=0;j<matrix[0].size();j++)
+           {
+               if(i<j)
+               swap(matrix[i][j],matrix[j][i]);
+           }
+       }
+    for(int i=0;i<matrix.size();i++)
+        reverse(matrix[i].begin(),matrix[i].end());
+        
     }
 };
