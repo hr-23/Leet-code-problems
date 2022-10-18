15ef51ffa0c6a250e299d1d014ce0857f78ce8a9
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-10-18 09:40:17 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (10.21%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool ispair(char o,char c)
+    {
+        if(o=='(' && c==')')return true;
+        if(o=='{' && c=='}')return true;
+        if(o=='[' && c==']')return true;
+        return false;
+    }
+    bool isValid(string s) {
+     stack<char>st;
+
+        if(s.size()<=1)return false;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='[' || s[i]=='{' || s[i]=='(')st.push(s[i]);
+            else 
+            {
+                if(!st.empty() && ispair(st.top(),s[i]))st.pop();
+                else return false;
+            }
+        }
+        if(st.empty())return true;
+        return false;
+    }
+};
