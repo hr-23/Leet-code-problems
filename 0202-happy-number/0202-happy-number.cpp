99413948dc3cb9cd91df8a455e01a00ca652485b
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-10-18 09:30:31 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (25.89%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool isHappy(int n) {
-         unordered_set<int>s;
+     unordered_set<int>s;
         
         while(n!=1)
         {
-            if(s.find(n)==s.end())
-                s.insert(n);
+           if(s.find(n)==s.end())
+               s.insert(n);
             else return false;
             int sum=0;
-            while(n!=0)
+            while(n)
             {
                 sum+=pow(n%10,2);
                 n/=10;
