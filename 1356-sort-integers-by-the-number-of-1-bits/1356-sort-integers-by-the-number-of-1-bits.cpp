ea5c3ec1858a2f33e878688d89f77e807d3318ae
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-10-19 20:40:17 +0530

    Time: 24 ms (30.61%), Space: 10.7 MB (29.96%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -6,11 +6,21 @@ public:
         if(a.second==b.second)return a.first<b.first;
         return false;
     }
+    static int cb(int a)
+    {
+        int cnt=0;
+        while(a)
+        {
+            a=a&(a-1);
+            cnt++;
+        }
+        return cnt;
+    }
     vector<int> sortByBits(vector<int>& nums) {
       vector<pair<int,int>>v;
         for(int i=0;i<nums.size();i++)
         {
-            int c=__builtin_popcount(nums[i]);
+            int c=cb(nums[i]);
             v.emplace_back(nums[i],c);
         }
         sort(v.begin(),v.end(),cmp);
