46d11ca2d7283922a79bb415a6f29b011adb5fa4
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-10-19 11:23:36 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (90.73%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // dutch national flar algorithm 
-        // three clours , three variables , 
-        // low , mid and high 
-        // always here mid plays an important role 
-   /* int l=0,mid=0,high=nums.size()-1;
-        while(mid<=high)
-        {
-            if(nums[mid]==0)swap(nums[l++],nums[mid++]);
-            else if(nums[mid]==1)mid++;
-            else if(nums[mid]==2)swap(nums[mid],nums[high--]);*/
-        
-        int high=nums.size()-1,low=0,mid=0;
+     // dutch natioanl flag algorithm 
+        // uses three colours , three variables 
+        // low,mid,high 
+        // mid plays a key role 
+        int low=0,mid=0,high=nums.size()-1;
         while(mid<=high)
         {
             if(nums[mid]==0)swap(nums[mid++],nums[low++]);
             else if(nums[mid]==1)mid++;
-            else if(nums[mid]==2)swap(nums[high--],nums[mid]);
+            else if(nums[mid]==2)swap(nums[mid],nums[high--]);
         }
-        
+       
     }
 };
