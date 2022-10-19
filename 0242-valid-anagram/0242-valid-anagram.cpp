fd014cd208f3daad8c1950489d0a837b32354125
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-10-19 18:00:06 +0530

    Time: 26 ms (31.11%), Space: 7.5 MB (13.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-         if (s.length() != t.length()) return false;
-        int n = s.length();
-        unordered_map<char, int> counts;
-        for (int i = 0; i < n; i++) {
-            counts[s[i]]++;
-            counts[t[i]]--;
+       if(s.length()!=t.length())return false;
+        unordered_map<char,int>m;
+        for(int i=0;i<s.length();i++)
+        {
+            m[s[i]]++;
+            m[t[i]]--;
         }
-        for (auto count : counts)
-            if (count.second) return false;
+        for(auto it:m)
+            if(it.second)return false;
         return true;
     }
 };
