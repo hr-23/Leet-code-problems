e19418f2e595ee7ec6c4217109dcd880c74ae56d
Harshaa Reddy G <83876846+hr-23@users.noreply.github.com>
2022-10-20 08:58:10 +0530

    Time: 40 ms (8.16%), Space: 12.8 MB (39.83%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
+    static bool cmp(pair<int,string>&a,pair<int,string>&b)
+    {
+        if(a.first==b.first)
+            return a.second<b.second;
+        return a.first>b.first;
+    }
     vector<string> topKFrequent(vector<string>& words, int k) {
-        map<string, int> cnt;
-        for (auto& w : words)
-            ++cnt[w];
-        
-        vector<vector<string>> bucket(words.size());
-        for (auto it : cnt)
-            bucket[it.second].push_back(it.first);
-        
-        vector<string> res;
-        for (int i = (int)bucket.size() - 1; k > 0 && i >= 0; i--) {
-            if (bucket[i].empty())
-                continue;
-            int n = min(k, (int)bucket[i].size());
-            res.insert(res.end(), bucket[i].begin(), bucket[i].begin() + n);
-            k -= n;
-        }
-        
+     unordered_map<string,int>m;
+        for(auto it:words)
+            m[it]++;
+        vector<pair<int,string>>v;
+        for(auto it:m)
+            v.push_back({it.second,it.first});
+         sort(v.begin(),v.end(),cmp);
+        vector<string>res;
+        for(int i=0;i<k;i++)
+            res.push_back(v[i].second);
         return res;
     
     }
